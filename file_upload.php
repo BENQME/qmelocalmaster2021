d11ac960d804ce4aca167d1829e3974023286521
Hetalben Vaghela <hetalvaghela106@gmail.com>
2021-08-11 21:06:33 +0530

    change code

--- file_upload.php
+++ file_upload.php
@@ -77,7 +77,34 @@ $up=move_uploaded_file($file_tmp, $filepath);
   //pushFile("your_username","your_personal_token","username/repository","repository_branch","path_of_targetfile_in_repository","base64_encoded_data");
 if($up) 
 {
-	pushFile("BENQME"," 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","BENQME/qmelocalmaster2021","main","uploads","base64_encoded_data","$fnm");
+	//pushFile("BENQME"," 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","BENQME/qmelocalmaster2021","main","uploads","base64_encoded_data","$fnm");
+	
+	
+	$file_git = $fnm;
+$data_git = array(
+'sha'=>file_get_contents("sha.txt"),
+'message'=>'image',
+'content'=> base64_encode($file_git),
+'committer'=> array(
+'name'=>'hetal',
+'email' => 'hetalvaghela106@gmail.com';
+);
+$data_string_git = json_encode($data_git);
+$updateUrl="https://api.github.com/repos/BENQME/qmelocalmaster2021/contents/uploads/$file_git";
+//$ch_git = curl_init('https://api.github.com/repos/YOUR_REPO/contents/wall.jpg');
+$ch_git = curl_init($updateUrl);
+curl_setopt($ch_git, CURLOPT_CUSTOMREQUEST, "PUT");
+curl_setopt($ch_git, CURLOPT_POSTFIELDS, $data_string_git);
+curl_setopt($ch_git, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch_git, CURLOPT_HTTPHEADER, array(
+'Content-Type: application/json',
+'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36',
+'Authorization: token PLACE_YOUR_PERSONAL_TOKEN_HERE'
+));
+$result_git = curl_exec($ch_git);
+echo $result_git;
+$p_git = json_decode($result_git);
+file_put_contents("sha.txt",$p_git->content->sha);
 echo "<img src=".$filepath." height=200 width=300 />";
 } 
 else 
