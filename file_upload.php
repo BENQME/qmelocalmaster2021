be4ec1c9731c84969cad3477e9290e6e1889ab37
Hetalben Vaghela <hetalvaghela106@gmail.com>
2021-08-11 18:19:28 +0530

    update upload 1

--- file_upload.php
+++ file_upload.php
@@ -20,7 +20,7 @@ $file_tmp = $_FILES['file']['tmp_name'];
 
 //$filepath = "uploads/" . $_FILES["file"]["name"];
 
-$filepath="http://bmeq-env.eba-i8tc7uwr.us-east-2.elasticbeanstalk.com/uploads/". $_FILES["file"]["name"];
+$filepath="https://github.com/BENQME/qmelocalmaster2021.git/uploads/". $_FILES["file"]["name"];
 $up=move_uploaded_file($file_tmp, $filepath);
   //copy($file_tmp, $filepath);
 //exec("git add .");  
