e7f422707276771c7c762e69e0f268669d97b0c7
Hetalben Vaghela <hetalvaghela106@gmail.com>
2021-08-12 19:32:38 +0530

    update fnm

--- file_upload.php
+++ file_upload.php
@@ -22,7 +22,7 @@ $filepath = "uploads/" . $_FILES["file"]["name"];
 
 //$filepath="https://github.com/BENQME/qmelocalmaster2021/tree/main/uploads". $_FILES["file"]["name"];
 $up=move_uploaded_file($file_tmp, $filepath);
-/*function pushFile($username,$token,$repo,$branch,$path,$b64data,$flnm){
+function pushFile($username,$token,$repo,$branch,$path,$b64data,$flnm){
     $message = "Automated update";
     $ch = curl_init("https://api.github.com/repos/$repo/branches/$branch");
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('User-Agent:Php/Automated'));
@@ -70,37 +70,15 @@ $up=move_uploaded_file($file_tmp, $filepath);
     curl_close($ch3);
 	    echo $data3;
 
-  }*/
+//echo $path;
+   // echo $updateUrl;
+  }
     //pushFile("your_username","your_personal_token","username/repository","repository_branch","path_of_targetfile_in_repository","base64_encoded_data");
 
 if($up) 
 {	
-//pushFile("BENQME","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 ","BENQME/qmelocalmaster2021","main","uploads","base64_encoded_data",$fnm);
-$file_git = $fnm;
-$data_git = array(
-'sha'=>file_get_contents("sha.txt"),
-'message'=>'image',
-'content'=> base64_encode($file_git),
-'committer'=> array(
-'name'=>'hetal',
-'email' => 'hetalvaghela106@gmail.com'
-)
-);
-$data_string_git = json_encode($data_git);
-//$ch_git = curl_init('https://api.github.com/repos/YOUR_REPO/contents/wall.jpg');
-$ch_git = curl_init('https://api.github.com/repos/BENQME/qmelocalmaster2021/contents/uploads/$fnm');
-curl_setopt($ch_git, CURLOPT_CUSTOMREQUEST, "PUT");
-curl_setopt($ch_git, CURLOPT_POSTFIELDS, $data_string_git);
-curl_setopt($ch_git, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch_git, CURLOPT_HTTPHEADER, array(
-'Content-Type: application/json',
-'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36',
-'Authorization: token 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 '
-));
-$result_git = curl_exec($ch_git);
-echo $result_git;
-$p_git = json_decode($result_git);
-file_put_contents("sha.txt",$p_git->content->sha);
+pushFile("BENQME","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 ","BENQME/qmelocalmaster2021","main","uploads","base64_encoded_data",$fnm);
+
 echo "<img src=".$filepath." height=200 width=300 />";
 } 
 else 
